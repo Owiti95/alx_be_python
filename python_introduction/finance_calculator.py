c481db0efb3d136c55ec9d0e43a6f6368da1f099
Owiti95 <hanningtonowiti@gmail.com>
2025-09-10 17:48:02 +0300

    .

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -9,5 +9,4 @@ projected_savings = annual_savings + (annual_savings * 0.05)
 
 
 print(f"Your monthly savings are {monthly_savings}")
-print(f"Your annual savings are {annual_savings}")
 print(f"Your projected savings are {projected_savings}")
